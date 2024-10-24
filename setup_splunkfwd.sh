f7d63888987dc5f62235b1a9071c7515bed668c1
Hoang NguyenDuc <52455578+tconqueror@users.noreply.github.com>
2024-10-24 15:31:59 +0700

    update splunk install script

--- setup_splunkfwd.sh
+++ setup_splunkfwd.sh
@@ -1,11 +1,11 @@
 cd /opt/
+chmod -R 777 /var/log/
 wget -O splunkforwarder-9.1.1-64e843ea36b1-linux-2.6-amd64.deb "https://download.splunk.com/products/universalforwarder/releases/9.1.1/linux/splunkforwarder-9.1.1-64e843ea36b1-linux-2.6-amd64.deb"
 dpkg -i splunkforwarder-9.1.1-64e843ea36b1-linux-2.6-amd64.deb
+wget -P /opt/splunkforwarder/etc/system/local/ https://raw.githubusercontent.com/tconqueror/ModSecurity-NGINX-setup/refs/heads/master/inputs.conf
+wget -P /opt/splunkforwarder/etc/system/local/ https://raw.githubusercontent.com/tconqueror/ModSecurity-NGINX-setup/refs/heads/master/outputs.conf
+wget -P /opt/splunkforwarder/etc/system/local/ https://raw.githubusercontent.com/tconqueror/ModSecurity-NGINX-setup/refs/heads/master/props.conf
+/opt/splunkforwarder/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd Vps@1234
+/opt/splunkforwarder/bin/splunk set deploy-poll 103.57.220.105:28089 -auth admin:Vps@1234
 
-wget -P /opt/splunkforwarder/etc/system/local/ -O https://raw.githubusercontent.com/tconqueror/ModSecurity-NGINX-setup/refs/heads/master/inputs.conf
-wget -P /opt/splunkforwarder/etc/system/local/ -O https://raw.githubusercontent.com/tconqueror/ModSecurity-NGINX-setup/refs/heads/master/outputs.conf
-wget -P /opt/splunkforwarder/etc/system/local/ -O https://raw.githubusercontent.com/tconqueror/ModSecurity-NGINX-setup/refs/heads/master/props.conf
-/opt/splunkforwarder/bin/splunk set deploy-poll 103.57.220.105:28089
-chmod -R 777 /var/log/
-
-/opt/splunkforwarder/bin/splunk start --accept-license
+/opt/splunkforwarder/bin/splunk restart
